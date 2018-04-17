3985700bbf23c28e745de4dd96d7589506e3610c
Satoshi Tsuchiya <mdhi3213@gmail.com>
2018-04-17 16:17:35 +0900

    管理画面でのの引き回し

--- public/php/gz_admin_op.php
+++ public/php/gz_admin_op.php
@@ -10,9 +10,10 @@ foreach ($_POST['check'] as $a => $b) {
     $n = $db->exec("UPDATE table1 SET ope = 0 WHERE ban = $b");
     print "{$b}は非公開です<br>";
 }
+
+print "<p><a href='/?fn=gz_admin&page={$page}'>管理画面に戻る</a></p>";
+print "<p><a href='/?fn=gz&page={$page}'>通常画面に戻る</a></p>";
 ?>
-<p><a href='/?fn=gz_admin'>管理画面に戻る</a></p>
-<p><a href='/?fn=gz'>通常画面に戻る</a></p>
 
 </body>
 </html>
